9881c1c8bac0283553d6db5d8d30a6d4815dedfb
username-taken-512 <jonatan.engerdahl@gmail.com>
2022-04-13 19:57:26 +0200

    - Added db path for local testing

--- backend/apiRouter.js
+++ backend/apiRouter.js
@@ -6,7 +6,7 @@ router.use(express.urlencoded({ extended: true }));
 // For PostGre db
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'database-url',
+  connectionString: process.env.DATABASE_URL || 'postgres://qbuwogoactaler:789dbde528d3bca42c8642c82f0a6bf98f9364bc508733f7b6513cfc9af89caf@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/d4ceut2b8fdle3',
   ssl: {
     rejectUnauthorized: false
   }
@@ -15,7 +15,7 @@ const pool = new Pool({
 router.get('/dbTest', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM User');
+    const result = await client.query('SELECT * FROM users');
     const results = { 'results': (result) ? result.rows : null };
     res.json(results);
     client.release();
