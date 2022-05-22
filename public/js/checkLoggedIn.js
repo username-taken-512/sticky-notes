43f91fc16e0105fc5578482aed8ba61545742aa3
Felix <felixkivioja95@gmail.com>
2022-05-22 16:41:10 +0200

    changed file name on frontlogin.js

---
+++ public/js/checkLoggedIn.js
@@ -0,0 +1,36 @@
+// ### Check if logged in 
+let refreshInterval;
+
+checkLoginStatus();
+
+async function checkLoginStatus() {
+
+  if (!window.localStorage.getItem('refreshToken')) window.location.href = "/login.html";
+
+  try {
+    result = await (response = await fetch('/api/users/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        token: window.localStorage.getItem('refreshToken')
+      })
+    })).json();
+  } catch (error) {
+    console.log(error);
+  }
+
+  switch (response.status) {
+    case 200:
+      window.localStorage.setItem('accessToken', result.accessToken);
+      break;
+    case 500:
+      alert('General error');
+      break;
+    case 403:
+      window.localStorage.setItem('accessToken', '');
+      window.localStorage.setItem('accessToken', '');
+      window.location.href = "/login.html"
+      break;
+  }
+
+}
